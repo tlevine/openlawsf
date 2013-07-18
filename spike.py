c6b464d28c3a5fe310bd17a6873dbe850d13d8cc
Thomas Levine <occurrence@thomaslevine.com>
2013-07-17 20:53:30 -0700

    download and cache

--- spike.py
+++ spike.py
@@ -7,25 +7,7 @@ def set_cookie():
     s.get('http://www.amlegal.com/nxt/gateway.dll?f=templates&fn=default.htm&vid=amlegal:sanfrancisco_ca')
     return s
 
-def cache(s, url):
-    'Given a session, get or cache the url.'
-    try:
-        os.mkdir('gateway')
-    except OSError:
-        pass
-    filename = os.path.join('gateway', re.sub(r'', '', url))
-
-    print filename
-    if os.path.isfile(filename):
-        return open(filename).read()
-    else:
-        handle = open(filename, 'w')
-        r = s.get(url)
-        handle.write(r.text)
-        handle.close()
-        return r.text
-
-def get(url, cachedir = '.'):
+def get(sess, url, cachedir = 'downloads'):
     'Download a web file, or load the version from disk.'
     tmp1 = url.replace('http://www.amlegal.com/nxt/gateway.dll/California/', '')
     tmp2 = [cachedir] + filter(None, tmp1.split('/'))
@@ -41,11 +23,12 @@ def get(url, cachedir = '.'):
     # Download
     if not os.path.exists(local_file):
        print 'Downloading and saving %s' % url
-       urlretrieve(url, filename = local_file)
-       randomsleep(1, 0.5)
+       r = sess.get(url)
+       handle = open(local_file, 'w')
+       handle.write(r.text.encode('utf-8'))
 
-    return open(local_file).read()
+    return open(local_file).read().decode('utf-8')
 
 s = set_cookie()
-cache(s, 'http://www.amlegal.com/nxt/gateway.dll/California/administrative/administrativecode')
-cache(s, 'http://www.amlegal.com/nxt/gateway.dll/California/administrative/chapter3budgetprocedures')
+get(s, 'http://www.amlegal.com/nxt/gateway.dll/California/administrative/administrativecode')
+get(s, 'http://www.amlegal.com/nxt/gateway.dll/California/administrative/chapter3budgetprocedures')
